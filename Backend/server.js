f1fa437937e05f96f4b89100f8eadaab09c39fc9
Pratik Patil <Pratikpatil1311@gmail.com>
2024-09-05 20:01:46 +0530

    configuration done and connection of DB done

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,34 @@
-console.log("This is working and modifying in github")
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./DBconfig/DB')
+
+// Load environment variables from .env file (if using dotenv)
+dotenv.config();
+
+// Define the Express app
+const app = express();
+
+// Use the dependencies in the proper way
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Database connection
+connectDB()
+
+// Define CORS configuration
+const corsOptions = {
+    origin: 'http://localhost:5173/', // Replace with your frontend's domain
+    methods: 'GET,POST,PUT,DELETE', // Specify the allowed methods
+    allowedHeaders: 'Content-Type,Authorization', // Specify the allowed headers
+    credentials: true, // Enable cookies if needed
+};
+
+// Apply CORS middleware with the configuration
+app.use(cors(corsOptions));
+
+// Running the server
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
